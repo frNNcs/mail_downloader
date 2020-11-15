68b38216a0a804be1912cc2567ac25440fcc4a94
francisco <frnncs@gmail.com>
2020-11-15 19:02:03 -0300

    new features

--- test.py
+++ test.py
@@ -10,10 +10,14 @@ import datetime
 from dateutil import parser
 from console_progressbar import ProgressBar
 import json
+import email
+from bs4 import BeautifulSoup
+
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-MAIL = 'jormarcdis@gmail.com'
+# MAIL = 'jormarcdis@gmail.com'
+MAIL = 'oficinavirtual@edelap.com.ar'
 QUERY = [
     'from:{}'.format(MAIL),
     # 'has:attachment',
@@ -45,19 +49,30 @@ def main():
     messages = results.get('messages', [])
 
     i = 0
-    pb = ProgressBar(total=len(messages), decimals=3, length=50, fill='#', zfill='-')
+    pb = ProgressBar(total=len(messages), decimals=3, length=50, fill='#', zfill='-',prefix='0', suffix=len(messages))
 
     if len(messages) > 0:
-        mail_folder = str(os.getcwd()) + '/' + MAIL + '/'
+        mail_folder = str(os.getcwd()) + '/media/' + MAIL + '/'
+
         try:
             os.mkdir(mail_folder)
         except:
             print('Carpeta existente')
 
+        mail_raw_folder = mail_folder + 'mails/'
         try:
-            mails_downloaded_file = open(mail_folder + "mails_downloaded.json", "r+")
+            os.mkdir(mail_raw_folder)
+
+        except:
+            print('Carpeta de mails existente')
+
+        try:
+            mails_downloaded_file = open(mail_folder + "mails_downloaded.json", "r")
             mails_downloaded = mails_downloaded_file.read()
+            mails_downloaded = json.loads(mails_downloaded)
             mails_downloaded = mails_downloaded['ids']
+            if mails_downloaded == None:
+                mails_downloaded = []
             mails_downloaded_file.close()
         except:
             mails_downloaded = []
@@ -65,6 +80,8 @@ def main():
         for message in messages:
             i += 1
             pb.print_progress_bar(i)
+
+            # Descargar adjuntos.
             if not message['id'] in mails_downloaded:
                 msg = service.users().messages().get(userId='me', id=message['id']).execute()
 
@@ -76,40 +93,54 @@ def main():
 
                 parts = msg.get('payload').get('parts')
                 all_parts = []
-                for p in parts:
-                    if p.get('parts'):
-                        all_parts.extend(p.get('parts'))
+                if parts:
+                    for p in parts:
+                        if p.get('parts'):
+                            all_parts.extend(p.get('parts'))
+                        else:
+                            all_parts.append(p)
+
+                    att_parts = [p for p in all_parts if 'attachmentId' in p['body']]
+                    filenames = [p['filename'] for p in att_parts]
+
+                    folder =  mail_folder + date + ' - ' + msg['id']
+                    try:
+                        os.mkdir(folder)
+                    except:
+                        mails_downloaded.append(message['id'])
                     else:
-                        all_parts.append(p)
+                        for part,filename in zip(att_parts,filenames):
+                            data = part['body'].get('data')
+                            attachmentId = part['body'].get('attachmentId')
+                            if not data:
+                                att = service.users().messages().attachments().get(
+                                        userId='me',
+                                        id=attachmentId,
+                                        messageId=message['id']).execute()
+                                data = att['data']
 
-                att_parts = [p for p in all_parts if 'attachmentId' in p['body']]
-                filenames = [p['filename'] for p in att_parts]
+                            str_file = base64.urlsafe_b64decode(data.encode('UTF-8'))
+                            f = open(folder + '/' + filename, "wb")
+                            f.write(str_file)
+                            f.close()
 
-                folder =  mail_folder + date + ' - ' + msg['id']
-                try:
-                    os.mkdir(folder)
-                except:
-                    mails_downloaded['ids'].append(message['id'])
-                else:
-                    for part,filename in zip(att_parts,filenames):
-                        data = part['body'].get('data')
-                        attachmentId = part['body'].get('attachmentId')
-                        if not data:
-                            att = service.users().messages().attachments().get(
-                                    userId='me',
-                                    id=attachmentId,
-                                    messageId=message['id']).execute()
-                            data = att['data']
+                # Descargar emails.
+                message = service.users().messages().get(userId='me', id=message['id'], format='raw').execute()
+                msg_str = base64.urlsafe_b64decode(message['raw'].encode("utf-8"))
+                mime_msg = email.message_from_string(msg_str.decode("utf-8", "replace"))
+                html_mail = base64.urlsafe_b64decode(mime_msg._payload)
 
-                        str_file = base64.urlsafe_b64decode(data.encode('UTF-8'))
-                        f = open(folder + '/' + filename, "wb")
-                        f.write(str_file)
-                        f.close()
-                    mails_downloaded['ids'] = mails_downloaded['ids'].append(message['id'])
+                soup = BeautifulSoup(html_mail, 'html.parser')
+                tds = soup.find_all('td')
+                f = open(mail_raw_folder + date + ' - ' + msg['id'] + '.html', "wb")
+                f.write(html_mail)
+                f.close()
+                mails_downloaded.append(message['id'])
 
             mails_downloaded_file = open(mail_folder + '/' + "mails_downloaded.json", "w")
-            mails_downloaded_file.write({'ids': mails_downloaded})
+            mails_downloaded_file.write(json.dumps({'ids': mails_downloaded}))
             mails_downloaded_file.close()
+
     else:
         print('No se han encontrado mails')
 
